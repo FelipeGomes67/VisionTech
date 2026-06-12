42bdbe8c8ce9dac1e0aa399472e9b1efc289a009
Felipe Gomes <felipegomes8819@gmail.com>
2026-06-12 10:54:59 -0300

    Scaffold Feito

---
+++ VisionTech/Models/Categorium.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace VisionTech.Models;
+
+public partial class Categorium
+{
+    [Key]
+    [StringLength(40)]
+    [Unicode(false)]
+    public string IdCategoria { get; set; } = null!;
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Nome { get; set; } = null!;
+
+    [InverseProperty("IdCategoriaNavigation")]
+    public virtual ICollection<Produto> Produtos { get; set; } = new List<Produto>();
+}
